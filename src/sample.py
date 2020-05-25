d0257b646ddcd193c9479b6b98ba579e00e71ab8
psenna <pedrosennapsc@gmail.com>
2020-05-25 19:43:06 -0400

    Get feature detector from env

--- src/sample.py
+++ src/sample.py
@@ -1,4 +1,5 @@
 import cv2 as cv
+import os
 
 class Sample:
     def __init__(self, data, label):
@@ -6,10 +7,38 @@ class Sample:
         self.label = label
         self.feature_vector = []
         self.feature_vector_type = ''
+        self.keypoints = []
+        self.descriptors = []
+
+    def find_keypoits(self):
+        keypoin_detector_type = os.getenv('KEYPOINT_DETECTOR_TYPE')
+
+        if (keypoin_detector_type == None):
+            keypoin_detector_type = "SIFT"
+
+        if (keypoin_detector_type == "DENSE"):
+            dense_detector_step = os.getenv("DENSE_DETECTOR_STEP")
+            if(dense_detector_step == None):
+                dense_detector_step = 10
+            if(isinstance(dense_detector_step, str)):
+                dense_detector_step = int(dense_detector_step)
+
+            self.keypoints = [cv.KeyPoint(x, y, dense_detector_step) for y in range(0, self.data.shape[0], dense_detector_step)
+                  for x in range(0, self.data.shape[1], dense_detector_step)]
+            return
+
+        sift_detector = cv.xfeatures2d_SIFT.create()
+        self.keypoints = sift_detector.detect(self.data)
+        return
+
+
 
     def extract_sift(self):
-        ver = cv.getVersionString()
-        sift = cv.xfeatures2d_SIFT.create()
-        kp, des = sift.detectAndCompute(self.data, None)
 
-        return kp
+        if(len(self.keypoints) == 0):
+            self.find_keypoits()
+
+        sift = cv.xfeatures2d_SIFT.create()
+        self.descriptors = sift.compute(self.data, self.keypoints)
+
+        return
--- src/sample_test.py
+++ src/sample_test.py
@@ -2,14 +2,65 @@ import unittest
 
 class SampleTest(unittest.TestCase):
 
-    def test_extrair_descritor_sift(self):
+    def test_find_dense_keypoints(self):
         import cv2 as cv
+        import os
         from src.sample import Sample
-        ver = cv.getVersionMajor()
+
+        os.environ['KEYPOINT_DETECTOR_TYPE'] = "DENSE"
+
         img = cv.imread('../data/gopher.jpg', cv.IMREAD_GRAYSCALE)
         sample = Sample(img, 1)
-        kp = sample.extract_sift()
-        self.assertEqual(len(kp), 1613)
+        sample.find_keypoits()
+        self.assertEqual(len(sample.keypoints), 6942)
+
+    def test_find_dense_keypoints_with_step(self):
+        import cv2 as cv
+        import os
+        from src.sample import Sample
+
+        os.environ['KEYPOINT_DETECTOR_TYPE'] = "DENSE"
+        os.environ['DENSE_DETECTOR_STEP'] = "50"
+
+        img = cv.imread('../data/gopher.jpg', cv.IMREAD_GRAYSCALE)
+        sample = Sample(img, 1)
+        sample.find_keypoits()
+        self.assertEqual(len(sample.keypoints), 288)
+
+    def test_find_sift_keypoints(self):
+        import cv2 as cv
+        import os
+        from src.sample import Sample
+
+        os.environ['KEYPOINT_DETECTOR_TYPE'] = "SIFT"
+
+        img = cv.imread('../data/gopher.jpg', cv.IMREAD_GRAYSCALE)
+        sample = Sample(img, 1)
+        sample.find_keypoits()
+        self.assertEqual(len(sample.keypoints), 1613)
+
+    def test_extraxt_sift_descriptor(self):
+        import cv2 as cv
+        from src.sample import Sample
+
+        img = cv.imread('../data/gopher.jpg', cv.IMREAD_GRAYSCALE)
+        sample = Sample(img, 1)
+        sample.extract_sift()
+        self.assertEqual(len(sample.keypoints), 1613)
+        self.assertEqual(sample.descriptors[1].shape, (1613, 128))
+
+    def test_extraxt_sift_descriptor_with_dense_detector(self):
+        import cv2 as cv
+        import os
+        from src.sample import Sample
+
+        os.environ['KEYPOINT_DETECTOR_TYPE'] = "DENSE"
+
+        img = cv.imread('../data/gopher.jpg', cv.IMREAD_GRAYSCALE)
+        sample = Sample(img, 1)
+        sample.extract_sift()
+        self.assertEqual(len(sample.keypoints), 6942)
+        self.assertEqual(sample.descriptors[1].shape, (6942, 128))
 
 
 if __name__ == '__main__':
